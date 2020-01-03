5f1b284fdfc535f336039c04da56290b1d93f696
Tanay Saxena <tanay.saxena002@gmail.com>
2020-01-03 22:44:25 +0530

    File

---
+++ Loop statement/574-optimized-number-pattern.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int no;
+    printf("Enter no:- ");
+    scanf("%d",&no);
+    for(int i=1;i<=no;i++)
+    {
+        for(int j=1;j<=no;j++)
+        {
+            if(i<j)
+            {
+                printf(" ");
+            }
+            else//
+            {
+                printf("%d",i);
+            }
+            
+        }
+    
+    printf("\n");
+    }
+    getch();
+}
